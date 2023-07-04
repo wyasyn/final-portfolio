f8951f8de382e03f30b66b11f3ffb9376dacfad0
Yasin Walum <ywalum@gmail.com>
2023-07-04 14:57:11 +0300

    added loader

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
+import React, {useState, useEffect} from "react";
 import {Footer, Navbar} from "./components";
 import {Routes, Route} from "react-router-dom";
 import {Home, About, Services, Portfolio, Blog, Contact, NotFound} from "./pages";
+import RingLoader from "react-spinners/RingLoader";
 
 function App() {
+  const [loading, setLoading] =useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(()=> {
+      setLoading(false);
+    },2000)
+  }, [])
+
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home /> } />
-        <Route path="/about" element={<About /> } />
-        <Route path="/services" element={<Services /> } />
-        <Route path="/portfolio" element={<Portfolio /> } />
-        <Route path="/blog" element={<Blog /> } />
-        <Route path="/contact" element={<Contact /> } />
-        <Route path="*" element={<NotFound /> } />
-      </Routes>
-      <Footer />
+ <>
+  {
+    loading ?
+  
+    <div className="loader">
+      <div className="loader-content">
+      <RingLoader
+        color={"#f00946"}
+        loading={loading}
+        size={150}
+        />
+        <h1>Yasin Walum</h1>
+      </div>
     </div>
+    :
+    <div className="App">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home /> } />
+      <Route path="/about" element={<About /> } />
+      <Route path="/services" element={<Services /> } />
+      <Route path="/portfolio" element={<Portfolio /> } />
+      <Route path="/blog" element={<Blog /> } />
+      <Route path="/contact" element={<Contact /> } />
+      <Route path="*" element={<NotFound /> } />
+    </Routes>
+    <Footer />
+  </div>
+
+  }
+ </>
   );
 }
 
