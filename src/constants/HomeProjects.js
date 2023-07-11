569e956e3cb5a8bd9b866853663c88327f75a0bc
Yasin Walum <ywalum@gmail.com>
2023-07-11 20:34:27 +0300

    changed description

--- src/constants/HomeProjects.js
+++ src/constants/HomeProjects.js
@@ -1,25 +1,25 @@
 export const HomeProjects = [
     {
         title: "Personal Portfolio",
-        description: "my personal site",
+        description: "website",
         img: "img/landing-page.jpg",
         url: "https://yasyn.netlify.app",
     },
     {
-        title: "Blogr Landing page",
-        description: "business web site",
+        title: "Blogr Home page",
+        description: "website",
         img: "img/blogr.jpg",
         url: "https://blogr256.netlify.app/",
     },
     {
-        title: "App Design With CSS",
-        description: "css skills show off",
+        title: "CSS Master",
+        description: "website",
         img: "img/app.jpg",
         url: "https://cerulean-scone-e8159c.netlify.app/",
     },
     {
         title: "Huddle Website",
-        description: "grid experience shown",
+        description: "website",
         img: "img/hudle.jpg",
         url: "https://huddle256.netlify.app/",
     },
